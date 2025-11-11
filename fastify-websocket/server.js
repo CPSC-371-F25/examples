762ee224aa30770d07c9deab8132820c64b5d1cb
Bridger Herman <bridger.g.herman@gmail.com>
2025-11-11 18:13:58 -0500

    Push websocket updates

--- fastify-websocket/server.js
+++ fastify-websocket/server.js
@@ -12,12 +12,37 @@ fastify.register(require('@fastify/websocket'));
 fastify.register(async (fastify) => {
     // Define a route for websocket to communicate on
     fastify.get('/connect', { websocket: true }, (socket, request) => {
-        console.log('new WebSocket client connected!');
-        socket.send('Connected to Fastify WebSocket server');
+        console.log('New WebSocket client connected!');
+        let initMsg = {
+            type: 'message',
+            name: 'Server',
+            data: 'Connected to Fastify WebSocket server',
+        };
+        socket.send(JSON.stringify(initMsg));
+
+        // Handle incoming messages from server
+        // yeah this would be tons better with typescript but in the
+        // interest of simplicity here we are..
+        // Message schema:
+        // {
+        //      "type": "message" | "coord",
+        //      "name": "your name",
+        //      "data": "some message" | [x, y] coord
+        // }
         socket.on('message', (message) => {
-            const text = message.toString();
-            console.log('new message from client: ' + text);
-            socket.send(`Your message was: ${text}`);
+            let msgObj = JSON.parse(message.toString());
+            let msgType = msgObj.type;
+            if (msgType == 'message') {
+                console.log(`${msgObj.name} says: "${msgObj.data}"`);
+                let outMsg = {
+                    type: 'message',
+                    name: 'Server',
+                    data: `Hello ${msgObj.name}`,
+                };
+                socket.send(JSON.stringify(outMsg));
+            } else if (msgType == 'coord') {
+                // TODO
+            }
         });
     });
 });
